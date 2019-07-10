051eb46d897b2d03c3b10cfcdf0fd06b23549a77
enrico76 <enricodip@gmail.com>
2019-07-10 18:38:10 +0200

    stampa in console proprietà oggetto + valore

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 // Creare un oggetto che descriva uno studente con le seguenti proprietà:
 //  nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà.
+
+// creazione oggetto con le proprietà richieste
+var student = {
+  "nome" : "giovanni",
+  "cognome" : "rossi",
+  "età" : "25"
+};
+// console.log(student.cognome);
+
+for (var listaprop in student) {
+  console.log(listaprop, ":"+ student[listaprop]);
+}
